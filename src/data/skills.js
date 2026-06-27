ec60e59fb7ca51d1547e9b64e5046bcffd7ec947
juankuri <147097052+juankuri@users.noreply.github.com>
2026-06-27 14:13:00 -0600

    feat: astro layout and components

---
+++ src/data/skills.js
@@ -0,0 +1,29 @@
+export const technicalSkills = [
+    {
+        title: "Programming Languages",
+        description: "Java, JavaScript, SQL, Python"
+    },
+    {
+        title: "Backend",
+        description: "Spring Boot, Express.js"
+    },
+    {
+        title: "Frontend",
+        description: "Astro, HTML, CSS"
+    }
+];
+
+export const softSkills = [
+    {
+        title: "Clear communication",
+        description: "I write code that works and stays working long after it's done"
+    },
+    {
+        title: "Conflict Resolution",
+        description: "I listen first, then build what actually solves the problem at hand"
+    },
+    {
+        title: "Adaptability",
+        description: "I ship Products that users want to use and keep using every day"
+    }
+];
