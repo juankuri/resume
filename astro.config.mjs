aed09f5568d3f2a746bea44ebbbda2b5da9e87b3
juankuri <147097052+juankuri@users.noreply.github.com>
2026-06-27 16:00:14 -0600

    redo astro site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-// import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-//   site: "https://juankuri.pages.dev",
+  site: "https://juankuri.pages.dev",
   integrations: [sitemap()]
 });
